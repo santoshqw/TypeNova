f8d8a2287eb29500c1869505ce156724c20eb733
roman <adhikariroman01m@gmail.com>
2026-02-22 22:21:44 +0545

    feat: integrate Tailwind CSS and add routing
    - Updated package.json to include Tailwind CSS and React Router dependencies.
    - Set up routing in App.jsx with a new ProfilePage component.
    - Removed unused react.svg asset.
    - Added Tailwind CSS import in index.css.
    - Wrapped App component with BrowserRouter in main.jsx for routing support.
    - Created ProfilePage component to display user profile.
    - Updated vite.config.js to include Tailwind CSS plugin.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import {Routes, Route} from "react-router";
+
+import ProfilePage from './pages/user/ProfilePage';
+
 
 const App = () => {
   return (
-    <div>Initial project setup</div>
+    <>
+      <Routes>
+        <Route path="/profile" element ={<ProfilePage />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ Frontend/src/pages/user/ProfilePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProfilePage = () => {
+  return (
+    <div className='text-red-600'>ProfilePage</div>
+  )
+}
+
+export default ProfilePage
